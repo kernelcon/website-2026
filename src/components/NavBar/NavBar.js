32ffd01007b3106e1812cc07cc82c3e5f743969d
clevernyyyy <adam.t.schaal@gmail.com>
2025-08-10 18:36:11 -0500

    Fix venue header: Update navbar and footer to match dark mode styling, add conditional navbar transparency for home route

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,26 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
+import { withRouter } from "react-router-dom";
 import KillerLogo from "../../static/images/logos/kernelcon_white.png";
 import "./NavBar.scss";
-export default class NavBar extends Component {
-	static displayName = "NavBar";
 
-	render() {
-		return (
-			<div className="navbar">
-				<div className="container">
-					<a href="/">
-						<img
-							src={KillerLogo}
-							className="navbar-logo-k25"
-							height="30"
-							alt="kernelcon logo"
-						/>
-						
-						<p className="nav-dates">Training: Apr 7-8</p>
-						<p className="second-nav-dates">Conference: Apr 9-10</p>
-					</a>
-				</div>
+const NavBar = ({ location }) => {
+	const isHomeRoute = location.pathname === "/";
+	
+	return (
+		<div className={`navbar ${isHomeRoute ? 'navbar-transparent' : 'navbar-dark'}`}>
+			<div className="container">
+				<a href="/">
+					<img
+						src={KillerLogo}
+						className="navbar-logo-k25"
+						height="30"
+						alt="kernelcon logo"
+					/>
+					
+					<p className="nav-dates">Training: Apr 7-8</p>
+					<p className="second-nav-dates">Conference: Apr 9-10</p>
+				</a>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
+
+export default withRouter(NavBar);
